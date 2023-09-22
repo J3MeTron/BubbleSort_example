2851fd56cee4d6bd96142f55cbaf8953e97732f4
Артём Петров <alfa.mi@hotmail.com>
2023-09-22 12:25:46 +0300

    Minor update

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -10,16 +10,19 @@ public class BubbleSort {
 
     public static int[] bubbleSort(int[] arr) {
         int n = arr.length;
-        boolean swapped;
+        boolean swapped; // Переменная для отслеживания выполнения обменов
+        // Внешний цикл для общего количества проходов
         for (int i = 0; i < n - 1; i++) {
-            swapped = false;
+            swapped = false;// Изначально нет обменов на данной итерации
+            // Внутренний цикл для одного прохода
             for (int j = 0; j < n - 1 - i; j++) {
+                // Сравниваем текущий элемент со следующим
                 if (arr[j] > arr[j + 1]) {
                     // Обмен элементов, если текущий элемент больше следующего
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
+                    swapped = true;// Устанавливаем флаг, что был обмен
                 }
             }
             // Если на данной итерации не было обменов, массив уже упорядочен
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] sortedArr = BubbleSort.bubbleSort(ReadIntArrayFromKeyboard.readAndPrintIntArray());
-        System.out.print("\n\nОтсортированный массив быстрой сортировкой: ");
+        System.out.print("\n\nОтсортированный массив сортировкой пузырьком: ");
         for (int num : sortedArr) {
             System.out.print(num + " ");
         }
